e62899cbb088ae6641c79e63f34eb3057063ba8b
Marco Chavez <mchaveza@ia.com.mx>
2019-10-23 13:43:58 -0500

    Library version release 1.0.0

--- app/src/main/java/com/android/softpermissions/MainActivity.kt
+++ app/src/main/java/com/android/softpermissions/MainActivity.kt
@@ -1,28 +1,13 @@
 package com.android.softpermissions
 
-import android.Manifest
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.android.permissionlibrary.callbacks.PermissionCallback
-import com.android.permissionlibrary.managers.PermissionManager
 
-class MainActivity : AppCompatActivity(), PermissionCallback {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val permission = PermissionManager(this, this)
-        Thread.sleep(3000)
-        permission.requestSinglePermission(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
-    override fun onPermissionGranted(permissions: String) {
-        Log.d(MainActivity::class.java.name, "GRANTED")
-    }
-
-    override fun onPermissionDenied(permission: String) {
-        Log.e(MainActivity::class.java.name, "GRANTED")
-    }
 }
